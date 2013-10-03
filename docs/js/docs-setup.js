c6186ef2d2445d0bb0c2cae62aeba7f7d0b88fee
Christopher Hiller <chiller@badwing.com>
2013-10-03 15:53:13 -0700

    - mostly updating docs - adding a bit of extra information injected into change/undo/redo/etc handlers

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,37 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "decipher.history",
+      "shortName": "history",
+      "type": "overview",
+      "keywords": "angularjs api back button decipher do for has history it nothing of overview service sort that the thing to undo unless want with you"
+    },
+    {
+      "section": "api",
+      "id": "decipher.history.object:Watch",
+      "shortName": "Watch",
+      "type": "object",
+      "keywords": "addchangehandler addredohandler addreverthandler adds addundohandler after an api archived back batch be been calls can certain chain change changes decipher deepwatch docs each example execute executed executing expression fn for function functions handler handlers have history instance is made mapping matching methods name object of parameters provides redo redone remove removechangehandler removeredohandler removereverthandler removerollbackhandler removes removeundohandler resolve return revert reverted rollback rolled see service several so that the these this tied to undo undone unique upon using value values watch whenever with you"
+    },
+    {
+      "section": "api",
+      "id": "decipher.history.service:History",
+      "shortName": "History",
+      "type": "service",
+      "keywords": "$id $locals $on $rootscope $watch accumulated addchangehandler addrollbackhandler all allows an and angular angularjs any api archived array as at back bar batch batchended baz baz2 be bunch but by can canredo canundo certain change changed complete console context data decipher deepwatch defaults derp derp2 description descriptions designated each earliest entire es example execute executed executes exp expression expressions exps fn foo foos for forget frack frack2 frick full function have herp history hit html id if in index instance is isdefined issue js keeping keyed known last lazy lazyoptions lazywatches loading locals log map message model module mychangehandler myrollbackhandler name ng-click ng-disabled ng-model ng-show not object objects of one only optional options or particular please point pointer pointers property provides redo redoes register registered return revert reverts rollback rolled rolls run scope script see server service some stack string stuff text that the then this timeout to transaction type undo undos unregister update valid value values var via was watch watched watches watching watchobjs we whether which within you"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.js",
+    "history.js"
+  ]
+};
